6c040da8097e2443971731dd1ea6da0f1e240e92
Quentin <quentin.amathalilou@gmail.com>
2025-01-21 20:28:26 +0100

    Update ui.R

--- ui.R
+++ ui.R
@@ -5,7 +5,7 @@ fluidPage(
   titlePanel("Emissions de CO2 Emissions du volcan Mauna Loa"),
   sidebarPanel(
     h3("Quantité de CO2 émise par le volcan Mauna Loa à Hawaï de 1959 à 1997"),
-    sliderInput("Range", "séléction de la plage de temps (années)", 
+    sliderInput("Range", "séléction de la plage de temps (années)", #Slider avec comme plage de valeurs les années présente sur le dataset
                 min = 1959, max = 1997, value = c(1959, 1970), step = 1)
   ),
   mainPanel(
